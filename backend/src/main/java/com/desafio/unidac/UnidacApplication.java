ad4b8ddf40fb1b2ba843d12e763c35c501384497
Eduarda <duda.eduarda.ec@gmail.com>
2023-05-04 23:17:06 -0300

    criando o backend

---
+++ backend/src/main/java/com/desafio/unidac/UnidacApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.unidac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnidacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnidacApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/desafio/unidac/UnidacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.unidac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnidacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
